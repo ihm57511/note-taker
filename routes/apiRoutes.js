c7a8dcfb3f033097f1a98f873671cc10c2e15981
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-03-15 09:27:03 -0400

    active note functional

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,16 @@
 const db = require('../db/db.json');
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(db));
+    app.get('/api/notes', (req, res) => {
+        let id = 0;
+        for (const note of db) {
+            note.id = id++
+        };
+        res.json(db)});
 
     app.post('/api/notes', (req, res) => {
         db.push(req.body);
+        res.json(db);
     })
+
 }
